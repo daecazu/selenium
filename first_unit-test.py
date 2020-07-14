be5c4f56e3f3f66f76cd13dff4be76ce43680055
Daniel Eduardo Cañón <daniel.canon@invytec.com>
2020-07-14 11:26:47 -0500

    first commit

---
+++ first_unit-test.py
@@ -0,0 +1,23 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path = './chromedriver/chromedriver')
+        cls.driver.implicitly_wait(10)
+
+    def test_hello_world(self):
+        self.driver.get('https://www.invytec.com/')
+    
+    def test_visit_wikipedia(self):
+        self.driver.get('https://www.platzi.com/')
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2, testRunner=HTMLTestRunner(output= 'reportes', report_name = 'hello-world-report') )
