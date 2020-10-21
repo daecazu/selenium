87cc2738692c2a11eeb3f1ab19fe5dbc735dd715
Daniel Eduardo Cañón <daniel.canon@invytec.com>
2020-10-21 09:36:06 -0500

    handling alerts in the store

---
+++ alerts.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+
+class CompareProducts(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path = './chromedriver/chromedriver')
+        driver = self.driver
+        driver.get("http://demo-store.seleniumacademy.com/")
+        driver.maximize_window()
+
+    def test_compare_products_removal_alert(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+        search_field.clear()
+        search_field.send_keys('tee')
+        search_field.submit()
+        driver.find_element_by_class_name('link-compare').click()
+        driver.find_element_by_link_text('Clear All').click()
+        alert = driver.switch_to.alert
+        alert_text = alert.text
+
+        self.assertEqual('Are you sure you would like to remove all products from your comparison?', alert_text)
+
+        alert.accept()
+
+
+    def tearDown(self):
+        self.driver.implicitly_wait(3)
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2)
